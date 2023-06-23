74283398a4b00fc8d74237610f384366894a3b8a
justinlime <justinlime1999@gmail.com>
2023-06-22 20:59:31 -0500

    fixes

--- startup.sh
+++ startup.sh
@@ -4,12 +4,13 @@ sed -i "s/owner = 0/owner = $OWNER/g" ./config.txt
 sed -i 's/prefix = "@mention"/prefix = "'$PREFIX'"/g' ./config.txt
 sed -i 's/game = "DEFAULT"/game = "'$GAME'"/g' ./config.txt
 sed -i "s/status = ONLINE/status = $STATUS/g" ./config.txt
-sed -i "s/songinstatus = false/songinstatus = $SONGINSTATUS/g" ./config.txt
+sed -i "s/songinstatus=false/songinstatus=$SONGINSTATUS/g" ./config.txt
 sed -i 's/altprefix = "NONE"/altprefix = "'$ALTPREFIX'"/g' ./config.txt
 sed -i "s/help = help/help = $HELP/g" ./config.txt
+sed -i "s/npimages = false/npimages = $NPIMAGES/g" ./config.txt
 sed -i "s/stayinchannel = false/stayinchannel = $STAYINCHANNEL/g" ./config.txt
 sed -i "s/maxtime = 0/maxtime = $MAXTIME/g" ./config.txt
 sed -i "s/alonetimeuntilstop = 0/alonetimeuntilstop = $ALONETIMEUNTILSTOP/g" ./config.txt
-sed -i "s/updatealerts = true/updatealerts = $UPDATEALERTS/g" ./config.txt
-sed -i 's|lyrics.default = "A-Z Lyrics"|lyrics.default = "'"$LYRICSDEFAULT"'"|g' ./config.txt
+sed -i "s/updatealerts=true/updatealerts=$UPDATEALERTS/g" ./config.txt
+sed -i 's|lyrics.default = "A-Z Lyrics"|lyrics.default = "'$LYRICSDEFAULT'"|g' ./config.txt
 java -jar JMusicBot-0.3.9.jar
